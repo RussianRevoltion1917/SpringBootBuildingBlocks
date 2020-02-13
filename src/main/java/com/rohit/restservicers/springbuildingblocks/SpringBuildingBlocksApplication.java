77b600681608e76ddff62822245b1e3e8f2ba5c5
RussianRevoltion1917 <52358731+RussianRevoltion1917@users.noreply.github.com>
2020-02-13 11:06:52 +0530

    First Commit

---
+++ src/main/java/com/rohit/restservicers/springbuildingblocks/SpringBuildingBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.restservicers.springbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuildingBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuildingBlocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohit/restservicers/springbuildingblocks/SpringBuildingBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.restservicers.springbuildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildingBlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
