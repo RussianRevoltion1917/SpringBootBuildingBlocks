d0e566bfe36dee784e4612497515281eae6094c1
RussianRevoltion1917 <52358731+RussianRevoltion1917@users.noreply.github.com>
2020-02-13 19:14:28 +0530

    Second Commit

--- src/main/java/com/rohit/restservicers/springbuildingblocks/SpringBuildingBlocksApplication.java
+++ src/main/java/com/rohit/restservicers/springbuildingblocks/SpringBuildingBlocksApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBuildingBlocksApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBuildingBlocksApplication.class, args);
